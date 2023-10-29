9acb5d3cb316956729a16c4a8e86a138e6f7ff1b
AsifZaman777 <eramasif16@gmail.com>
2023-10-29 11:52:29 +0600

    advance promise chain

--- 5.promise chain.js
+++ 5.promise chain.js
@@ -1,69 +1,70 @@
-
-const cart= ["bag","makup kit","bag"];
+const cart = ["bag", "makeup kit", "bag"];
 
 createOrder(cart)
-.then((orderId)=>
-{
-    console.log(orderId+" has been created");
+  .then((orderId) => {
+    console.log(orderId + " has been created");
     return orderId;
-})
-.catch((err)=>
-{
-    console.log(err);  // thrown error from the createOrder promise
-})
-.then((orderId)=>
-{
-   return proceedToPayment(orderId);
-})
-.then((paymentInfo)=>
-{
-   console.log(paymentInfo);
-})
-.catch((err)=>
-{
-    console.log(err);   // error message from the proceedToPayment promise 
-})
+  })
+  .catch((err) => {
+    console.log("Cart is not valid"); // Error from the createOrder promise
+  })
+  .then((orderId) => {
+    return proceedToPayment(orderId);
+  })
+  .then((paymentInfo) => {
+    console.log(paymentInfo);  //show a message
+    return { orderId: paymentInfo.orderId, paymentStatus: paymentInfo.paymentStatus }; //pass some data to the down chain
+  })
+  .then(({ orderId, paymentStatus }) => {
+    return updateWallet(orderId, paymentStatus);
+  })
+  .then((updateWalletResolved) => {
+    console.log(updateWalletResolved);
+  })
+  .catch((err) => {
+    console.log(err); 
+  });
 
-
-//that catch will handle errors of its upper chain. Catch doesnt care about the down chain 
-
-function createOrder(cart) 
-{
-   return new Promise((resolve,reject)=>
-   {
-     //validate cart
-     //return orderId
-     if(!validateCart(cart))
-     {
-        const err = new Error("Cart is not validate"); //error throw
-        reject(err);
-     }
-     else{
-           const orderId=Math.floor(Math.random() * 1000);
-           //false timing to show async operation
-           setTimeout(()=>
-           {
-            resolve(orderId);
-           },2000)
-     }
-
-   });
+function createOrder(cart) {
+  return new Promise((resolve, reject) => {
+    if (!validateCart(cart)) {
+      const err = new Error("Cart is not valid"); // Error throw
+      reject(err);
+    } else {
+      const orderId = Math.floor(Math.random() * 1000);
+      setTimeout(() => {
+        resolve(orderId); //orderID will go to the next chain
+      }, 2000);
+    }
+  });
 }
 
-function proceedToPayment(orderId)
-{
-    return new Promise((resolve,reject)=>
-    {
-       orderId ?
-       setTimeout(()=>
-       {
-        resolve("Payment Successful");
-       },1000)  
-       : reject("You did not choose any product"); ///error message
-    });
+function proceedToPayment(orderId) {
+  return new Promise((resolve, reject) => {
+    if (orderId) {
+      setTimeout(() => {
+        var paymentStatus = true;
+        resolve({orderId,paymentStatus});  //this json will go the next chain
+        return console.log("Payment successful");  //always return after resolve//
+      }, 1000);
+    } else {
+      reject("You did not choose any product");
+    }
+  });
 }
 
-function validateCart(cart)
-{
-    return true;
+function updateWallet(orderId, paymentStatus) {
+  return new Promise((resolve, reject) => {
+    if (paymentStatus === true) {
+      setTimeout(() => {
+        resolve("Wallet updated of order ID: " + orderId);
+      }, 1000);
+    } else {
+      reject("No change in the wallet " + orderId);
+    }
+  });
+}
+
+function validateCart(cart) {
+  return true; 
 }
